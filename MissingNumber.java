2d03feb6917af1ede396b3823e28cbae548f6156
naveenchinthakayala19-dotcom <naveenchinthakayala19@gmail.com>
2026-08-07 13:30:55 +0530

    Add files via upload

---
+++ MissingNumber.java
@@ -0,0 +1,23 @@
+class MissingNumber{
+public static void main(String args[]){
+int ar[]=new int[] {1,2,6,7,8,9,10,4,3};
+int res=m1(ar);
+System.out.println(res);
+}
+private static int m1(int[] ar){
+for(int num=1;num<=10;num++){
+boolean flag=false;
+for(int i=0;i<9;i++){
+if(ar[i]==num){
+flag=true;
+break;
+}
+}
+if(flag==false){
+return num;
+
+}
+}
+return -1;
+}
+}
